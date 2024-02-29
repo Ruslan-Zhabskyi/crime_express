e2684c9e1c33171fe6adc539f4d7a3d1bbe3be02
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2024-02-29 21:38:13 +0000

    updated json location and report stores

--- src/server.js
+++ src/server.js
@@ -51,7 +51,7 @@ async function init() {
   });
   server.auth.default("session");
 
-  db.init();
+  db.init("json");
   server.route(webRoutes);
   await server.start();
   console.log("Server running on %s", server.info.uri);
