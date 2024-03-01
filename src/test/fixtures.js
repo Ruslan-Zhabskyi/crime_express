1939ab8eef0a7812c783b20e35c754a8c17400c3
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2024-03-01 18:25:56 +0000

    fixed issues in report store

--- src/test/fixtures.js
+++ src/test/fixtures.js
@@ -1,3 +1,4 @@
+export const serviceUrl = "http://localhost:3000";
 export const maggie = {
   firstName: "Maggie",
   lastName: "Simpson",
@@ -26,7 +27,31 @@ export const testUsers = [
   },
 ];
 
-export const Malahide = {
+export const Wicklow = {
+  name: "Wicklow",
+  latitude: 52.975,
+  longitude: -6.04944,
+};
+
+export const testLocations = [
+  {
+    name: "Malahide",
+    latitude: 53.45,
+    longitude: -6.16,
+  },
+  {
+    name: "Dublin",
+    latitude: 53.33306,
+    longitude: -6.24889,
+  },
+  {
+    name: "Drogheda",
+    latitude: 53.71889,
+    longitude: -6.34778,
+  },
+];
+
+export const malahideReport = {
   name: "Late Night Disturbance",
   category: "Anti Social Behavior",
   description: "Loud music and shouting heard from the property late at night, causing disturbance to the neighbors",
@@ -34,14 +59,26 @@ export const Malahide = {
   longitude: -6.16,
 };
 
-export const testLocations = [
+export const testReports = [
   {
-    title: "",
+    name: "Night Disturbance",
+    category: "Anti Social Behavior",
+    description: "Shouting heard late at night",
+    latitude: 53.2,
+    longitude: -6.36,
   },
   {
-    title: "",
+    name: "Interference with the property",
+    category: "Property Crimes",
+    description: "Burglary",
+    latitude: 50.2,
+    longitude: -5.36,
   },
   {
-    title: "",
+    name: "Hacking",
+    category: "Cyber Crimes",
+    description: "hacked home network",
+    latitude: 53.22,
+    longitude: -6.34,
   },
 ];
---
+++ src/test/models/users-model-test.js
@@ -0,0 +1,56 @@
+import { assert } from "chai";
+import { db } from "../../models/db.js";
+import { maggie, testUsers } from "../fixtures.js";
+import { assertSubset } from "../test-utils.js";
+
+suite("User Model tests", () => {
+  setup(async () => {
+    db.init("json");
+    await db.userStore.deleteAll();
+    for (let i = 0; i < testUsers.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      testUsers[i] = await db.userStore.addUser(testUsers[i]);
+    }
+  });
+
+  test("create a user", async () => {
+    const newUser = await db.userStore.addUser(maggie);
+    assertSubset(maggie, newUser);
+  });
+
+  test("delete all userApi", async () => {
+    let returnedUsers = await db.userStore.getAllUsers();
+    assert.equal(returnedUsers.length, 3);
+    await db.userStore.deleteAll();
+    returnedUsers = await db.userStore.getAllUsers();
+    assert.equal(returnedUsers.length, 0);
+  });
+
+  test("get a user - success", async () => {
+    const user = await db.userStore.addUser(maggie);
+    const returnedUser1 = await db.userStore.getUserById(user._id);
+    assert.deepEqual(user, returnedUser1);
+    const returnedUser2 = await db.userStore.getUserByEmail(user.email);
+    assert.deepEqual(user, returnedUser2);
+  });
+
+  test("delete One User - success", async () => {
+    await db.userStore.deleteUserById(testUsers[0]._id);
+    const returnedUsers = await db.userStore.getAllUsers();
+    assert.equal(returnedUsers.length, testUsers.length - 1);
+    const deletedUser = await db.userStore.getUserById(testUsers[0]._id);
+    assert.isNull(deletedUser);
+  });
+
+  test("get a user - bad params", async () => {
+    assert.isNull(await db.userStore.getUserByEmail(""));
+    assert.isNull(await db.userStore.getUserById(""));
+    assert.isNull(await db.userStore.getUserById());
+  });
+
+  test("delete One User - fail", async () => {
+    await db.userStore.deleteUserById("bad-id");
+    const allUsers = await db.userStore.getAllUsers();
+    assert.equal(testUsers.length, allUsers.length);
+  });
+});
