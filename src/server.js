05db78f76969a3fbad18ed4f90a7b67f391d4355
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2024-03-03 11:34:15 +0000

    -fixed bug in location mongo store. -added location test

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import Handlebars from "handlebars";
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
 import { accountsController } from "./controllers/accounts-controller.js";
+import { apiRoutes } from "./api-routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -54,6 +55,7 @@ async function init() {
   db.init("json");
   server.route(webRoutes);
   await server.start();
+  server.route(apiRoutes);
   console.log("Server running on %s", server.info.uri);
 }
 
