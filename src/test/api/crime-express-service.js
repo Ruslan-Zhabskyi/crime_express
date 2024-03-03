897f144a8fc33066a44415f1429a9f522b44c8b7
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2024-03-03 11:50:21 +0000

    extending API

--- src/test/api/crime-express-service.js
+++ src/test/api/crime-express-service.js
@@ -1,1 +1,12 @@
-export const crimeexpressService = {};
+import axios from "axios";
+
+import { serviceUrl } from "../fixtures.js";
+
+export const crimeexpressService = {
+  crimeexpressUrl: serviceUrl,
+
+  async createUser(user) {
+    const res = await axios.post(`${this.crimeexpressUrl}/api/users`, user);
+    return res.data;
+  },
+};
--- src/test/api/user-api-test.js
+++ src/test/api/user-api-test.js
@@ -1,8 +1,15 @@
 import { crimeexpressService } from "./crime-express-service.js";
+import { assert } from "chai";
+import { assertSubset } from "../test-utils.js";
+import { maggie } from "../fixtures.js";
 
 suite("User API tests", () => {
   setup(async () => {});
   teardown(async () => {});
 
-  test("create a user", async () => {});
+  test("create a user", async () => {
+    const newUser = await crimeexpressService.createUser(maggie);
+    assertSubset(maggie, newUser);
+    assert.isDefined(newUser._id);
+  });
 });
