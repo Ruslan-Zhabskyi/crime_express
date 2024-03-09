ddbc9a4d831c620199f55a6cdba1925f71a91c3c
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2024-03-09 15:19:59 +0000

    added option to upload images

---
+++ src/models/image-store.js
@@ -0,0 +1,29 @@
+import * as cloudinary from "cloudinary";
+import { writeFileSync } from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const credentials = {
+  cloud_name: process.env.cloudinary_name,
+  api_key: process.env.cloudinary_key,
+  api_secret: process.env.cloudinary_secret,
+};
+cloudinary.config(credentials);
+
+export const imageStore = {
+  getAllImages: async function () {
+    const result = await cloudinary.v2.api.resources();
+    return result.resources;
+  },
+
+  uploadImage: async function (imagefile) {
+    writeFileSync("./public/temp.img", imagefile);
+    const response = await cloudinary.v2.uploader.upload("./public/temp.img");
+    return response.url;
+  },
+
+  deleteImage: async function (img) {
+    await cloudinary.v2.uploader.destroy(img, {});
+  },
+};
